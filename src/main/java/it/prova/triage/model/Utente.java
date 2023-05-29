171145b5e0b9be8846fc6718bec7f5d019b6dffb
Gabriel Scarlat <gabrielscarlat36@gmail.com>
2023-05-29 11:21:45 +0200

    aggiunta del model

---
+++ src/main/java/it/prova/triage/model/Utente.java
@@ -0,0 +1,58 @@
+package it.prova.triage.model;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "utente")
+public class Utente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cognome")
+	private String cognome;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "dateCreated")
+	private LocalDate dateCreated;
+
+	// se non uso questa annotation viene gestito come un intero
+	@Enumerated(EnumType.STRING)
+	private StatoUtente stato;
+
+	@Builder.Default
+	@ManyToMany
+	@JoinTable(name = "utente_ruolo", joinColumns = @JoinColumn(name = "utente_id", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "ruolo_id", referencedColumnName = "ID"))
+	private Set<Ruolo> ruoli = new HashSet<>(0);
+
+}
